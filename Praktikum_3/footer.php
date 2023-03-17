d0667dec80f70a571289f0e9e597623f4617dc1a
zila03 <114472146+zila03@users.noreply.github.com>
2023-03-17 13:09:31 +0700

    Pekan 3

---
+++ Praktikum_3/footer.php
@@ -0,0 +1,7 @@
+<footer>
+        <p>Created by
+            <a href="">Naazila Alfa Syahrin &copy;2023</a>
+        </p>
+    </footer>
+</body>
+</html>
---
+++ Praktikum_3/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Student Activity Score - STTNF</h1>
+        <a href="">Home</a> | <a href="">Score</a> | <a href="">LOGIN</a>
+        <hr>
+    </header>
+
+    <footer>
+        <p>Created by
+            <a href="">Naazila Alfa Syahrin &copy;2023</a>
+        </p>
+    </footer>
+</body>
+</html>
