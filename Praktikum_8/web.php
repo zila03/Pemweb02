fe5823b8f596da2a5ff8aad385df8055c3fa05cf
zila03 <114472146+zila03@users.noreply.github.com>
2023-05-18 22:13:43 +0700

    Pekan 8

---
+++ Praktikum_8/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/kabar', function () {
+ return view('kondisi');
+});
+
+Route::get('/nilai', function () {
+    return view('nilai');
+   });
+
+   Route::get('/form', function () {
+    return view('form');
+   });
